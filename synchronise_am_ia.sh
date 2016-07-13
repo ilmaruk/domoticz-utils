8c1586fc17786332784e6c022e0f31cf6c04c3db
ilmaruk <ruggero.ferretti@qton.com>
2016-07-13 11:02:46 +0100

    Fixed path to pi home dir in shell script for synchronise_am_ia

--- synchronise_am_ia.sh
+++ synchronise_am_ia.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+PI_HOME="/home/pi"
 pushd $(dirname $0) > /dev/null
-. ~/.venv/domoticz-utils/bin/activate
+. "${PI_HOME}/.venv/domoticz-utils/bin/activate"
 python synchronise_am_ia.py
 deactivate
 popd > /dev/null
